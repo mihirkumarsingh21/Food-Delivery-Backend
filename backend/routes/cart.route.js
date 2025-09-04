e870ce344e2da0b10bf38c54fe20663ff3f396cc
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-09-04 21:28:48 +0530

    some updated cart route

--- backend/routes/cart.route.js
+++ backend/routes/cart.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authProtect } from "../middlewares/auth.middleware.js";
-import { authUserAddingProductCart, authUserUpdatingProductCart, authUserRemoveProductCart, authUserGettingProductCart } from "../controllers/cart.controller.js";
+import { authUserAddingProductCart, authUserUpdatingProductCart, authUserClearProductCart, authUserGettingProductCart, authUserRemoveProductCart ,authUserClearOneProductCart } from "../controllers/cart.controller.js";
 
 const router = express.Router();
 
@@ -8,6 +8,8 @@ router.post("/add-cart", authProtect, authUserAddingProductCart);
 router.get("/cart-list/:cartId", authProtect, authUserGettingProductCart);
 router.put("/update-cart/:cartId", authProtect, authUserUpdatingProductCart);
 router.delete("/remove-cart/:cartId", authProtect, authUserRemoveProductCart);
+router.delete("/clear-cart", authProtect, authUserClearProductCart);
+router.delete("/cart/:cartId/item/:productId", authProtect, authUserClearOneProductCart);
 
 
 export default router;
