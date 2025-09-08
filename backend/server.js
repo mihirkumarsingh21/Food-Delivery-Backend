381d63e1a136f71d2988734609d3a14cee3065bc
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-09-09 02:04:24 +0530

    some update in server

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,8 @@ import categoryRoute from "./routes/category.route.js";
 import foodItemsRoute from "./routes/food.product.route.js";
 import authUserProfile from "./routes/user.profile.route.js";
 import cartRoute from "./routes/cart.route.js";
+import orderRoute from "./routes/order.route.js";
+
 
 dotenv.config();
 const app = express();
@@ -16,14 +18,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
-// app.use("public",express.static("public"))
 
 app.use("/api/v1", authRoute);
 app.use("/api/v1/profiles", authUserProfile)
 app.use("/api/v1/food-category", categoryRoute);
 app.use("/api/v1/food-items", foodItemsRoute);
-
 app.use("/api/v1/carts", cartRoute);
+app.use("/api/v1/orders", orderRoute);
+
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
