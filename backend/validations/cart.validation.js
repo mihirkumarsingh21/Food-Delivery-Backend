5cb58198e68ba35d36b43167886557164cbc94f9
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-09-09 02:09:28 +0530

    somevalidation added to new field in cart

--- backend/validations/cart.validation.js
+++ backend/validations/cart.validation.js
@@ -1,20 +1,27 @@
 import Joi from "joi";
 
 export const cartSchemaValidation = Joi.object({
+    
         userId: Joi.string()
         .required(),
 
         items: Joi.array()
         .items( Joi.object({
+
             productId: Joi.string()
             .required(),
 
-            price: Joi.number()
-            .required(),
+            price: Joi.number(),
 
             quantity: Joi.number()
-            .min(1)
-        }) )
+            .min(1),
 
+            subTotal: Joi.number()
 
+        })),
+
+        totalAmount: Joi.number(),
+        address: Joi.string(),
+        orderStatus: Joi.string()
+        .default("pending")
 })
