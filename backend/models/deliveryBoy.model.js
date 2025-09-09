d53258535d8872a972f2c0667ce9be52b9674562
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-09-10 03:51:41 +0530

    delivery boy model

---
+++ backend/models/deliveryBoy.model.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const deliveryBoySchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Types.ObjectId,
+        ref: "User",
+    },
+
+   vehicleType: {
+        type: String,
+        enum: ["motorbike"],
+        default: "motorbike",
+        required: true
+   },
+
+   licenseNumber: {
+        type: String,
+        required: true
+   },
+
+   address: String,
+
+   phone: {
+        type: String,
+        required: true
+   },
+
+   rating: {
+        type: Number,
+        default: 0
+   },
+
+   activeStatus: {
+    type: Boolean,
+    default: false
+   }
+
+
+}, {timestamps: true})
+
+export const DeliveryBoy = mongoose.model("DeliveryBoy", deliveryBoySchema);
