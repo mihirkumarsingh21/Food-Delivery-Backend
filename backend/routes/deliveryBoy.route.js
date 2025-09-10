ab59d0513dffe31947643397d9039e3e7348183e
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-09-11 01:51:34 +0530

    Add role assignment and order assignment routes for delivery boys

--- backend/routes/deliveryBoy.route.js
+++ backend/routes/deliveryBoy.route.js
@@ -1,10 +1,16 @@
 import express from "express";
 import { authProtect } from "../middlewares/auth.middleware.js";
-import { addingDeliveryBoyDetails } from "../controllers/deliveryBoy.controller.js";
+import { addingDeliveryBoyDetails, givingDeliveryRole, ownerAssignOrderFoodToDeliveryBoy  } from "../controllers/deliveryBoy.controller.js";
+import { owner } from "../middlewares/owner.middleware.js";
+import { checkDeliveryRole } from "../middlewares/delivery.middleware.js";
 
 const router = express.Router();
 
 router.post("/adding-details", authProtect, addingDeliveryBoyDetails);
+router.patch("/role", authProtect, owner, givingDeliveryRole);
+// router.put("/update-order-status", checkDeliveryRole, deliveryBoyUpdaingOrderStatus);
+router.patch("/orders/:orderId/assign", authProtect, checkDeliveryRole, owner, ownerAssignOrderFoodToDeliveryBoy);
+
 
 
 export default router;
