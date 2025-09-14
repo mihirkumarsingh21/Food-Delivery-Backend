313b4916dece66b45ee1139e1bc5ff31bd452379
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-09-14 21:51:22 +0530

    Enhance order routes by adding sorting and filtering functionalities for owner access

--- backend/routes/order.route.js
+++ backend/routes/order.route.js
@@ -1,9 +1,9 @@
 
 import express from "express";
 import { authProtect } from "../middlewares/auth.middleware.js";   
-import { authUserGettingAllOrderFoodList, authUserGettingOrderFoodListBySearching, authUserMakingOrder, gettingOrderHistory } from "../controllers/order.controller.js";
+import { authUserGettingAllOrderFoodList, authUserGettingOrderFoodListBySearching, authUserMakingOrder, gettingOrderHistory, ownerFilterOrderFood} from "../controllers/order.controller.js";
 import { owner } from "../middlewares/owner.middleware.js";
-import { ownerUpdatingOrderStatus, ownerListedOrderFood } from "../controllers/order.controller.js";
+import { ownerUpdatingOrderStatus, ownerListedOrderFood, ownerSortedOrderFood, orderStatus } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
@@ -13,10 +13,10 @@ router.patch("/:orderId/history", authProtect, owner, gettingOrderHistory);
 router.get("/my", authProtect, authUserGettingAllOrderFoodList);
 router.get("/search/my", authProtect, authUserGettingOrderFoodListBySearching);
 
-
 router.get("/order-list", authProtect, owner, ownerListedOrderFood);
+router.get("/sorted-order-list", authProtect, owner, ownerSortedOrderFood);
+router.get("/filter-order-list", authProtect, owner, ownerFilterOrderFood);
 
-
-
+router.get("/deliveryBoy/:id/status", authProtect, owner, orderStatus);
 
 export default router;
