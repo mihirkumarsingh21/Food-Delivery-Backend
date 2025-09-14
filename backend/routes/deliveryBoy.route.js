6a7033fef016f36105b83e68012341c83358cb3e
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-09-14 21:50:56 +0530

    Refactor deliveryBoy routes to include order fetching and status updating functionality

--- backend/routes/deliveryBoy.route.js
+++ backend/routes/deliveryBoy.route.js
@@ -1,16 +1,17 @@
 import express from "express";
 import { authProtect } from "../middlewares/auth.middleware.js";
-import { addingDeliveryBoyDetails, givingDeliveryRole, ownerAssignOrderFoodToDeliveryBoy  } from "../controllers/deliveryBoy.controller.js";
+import { addingDeliveryBoyDetails, givingDeliveryRole, ownerAssignOrderFoodToDeliveryBoy, deliveryBoyFetchingAssignOrder,  deliveryBoyUpdatingOrderStatus  } from "../controllers/deliveryBoy.controller.js";
 import { owner } from "../middlewares/owner.middleware.js";
 import { checkDeliveryRole } from "../middlewares/delivery.middleware.js";
+import { orderStatus } from "../controllers/order.controller.js";
 
 const router = express.Router();
 
 router.post("/adding-details", authProtect, addingDeliveryBoyDetails);
 router.patch("/role", authProtect, owner, givingDeliveryRole);
-// router.put("/update-order-status", checkDeliveryRole, deliveryBoyUpdaingOrderStatus);
-router.patch("/orders/:orderId/assign", authProtect, checkDeliveryRole, owner, ownerAssignOrderFoodToDeliveryBoy);
-
-
+router.patch("/orders/:orderId/assign", authProtect, owner, checkDeliveryRole, ownerAssignOrderFoodToDeliveryBoy);
+router.get("/deliveryBoy/:id/orders", authProtect, owner, checkDeliveryRole, deliveryBoyFetchingAssignOrder);
+router.patch("/deliveryBoy/:id/order/status", authProtect, owner, deliveryBoyUpdatingOrderStatus);
+router.get("/:id/orderStatus", authProtect, owner, orderStatus);
 
 export default router;
